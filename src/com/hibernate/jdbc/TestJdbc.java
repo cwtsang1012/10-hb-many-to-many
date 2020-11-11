cbf52ca12818457b25c35603ae0acd8945b2845d
Grace Tsang <33336177+cwtsang1012@users.noreply.github.com>
2020-11-12 01:37:13 +0800

    first commit

---
+++ src/com/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,25 @@
+package com.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pwd = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pwd);
+			
+			System.out.println("Connection successful!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
